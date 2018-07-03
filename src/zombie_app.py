49d7ee023a2ee359e8d210ab99114b03783c06ad
Kevin Hoopes <kevin.alex.hoopes@gmail.com>
2018-07-02 22:03:13 -0500

    Created zombie_app.py

---
+++ src/zombie_app.py
@@ -0,0 +1,35 @@
+"""This module contains the logic behind the Zombie
+Adventure application.
+@author Kevin Hoopes
+@version 7/2/2018
+"""
+
+###########
+# IMPORTS #
+###########
+
+from random_generator import ItemGenerator, AmountGenerator, ItemAmountGenerator
+
+#############
+# CONSTANTS #
+#############
+
+WEAPONS_PATH = "../sample_resources/example_weapons.json"
+ENCOUNTERS_PATH = "../sample_resources/example_encounters.json"
+FOOD_PATH = "../sample_resources/example_food.json"
+GAS_PATH = "../sample_resources/example_gas.json"
+MATERIALS_PATH = "../sample_resources/example_materials.json"
+AMMO_PATH = "../sample_resources/example_ammo.json"
+
+####################
+# CLASS DEFINITION #
+####################
+
+class ZombieApp():
+
+    """An object to perform the application logic
+    when requested.
+    """
+
+    def __init__(self):
+        self.
